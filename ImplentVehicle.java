c5908907cf7ef707d112eed0c5a0b102ab80094e
Md.Imran Hossen <72429143+Imran1910276131@users.noreply.github.com>
2023-09-27 12:30:44 +0600

    Add files via upload
    Uploaded File manually

---
+++ ImplentVehicle.java
@@ -0,0 +1,9 @@
+class ImplentVehicle implements Car,Bus{
+    public void method2() {
+             System.out.println("I am from Bus");
+    }
+    public void method1() {
+             System.out.println("I am from Car");
+    }
+    
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        ImplentVehicle obj=new ImplentVehicle();
+        obj.method1();
+        obj.method2();
+    }
+}
